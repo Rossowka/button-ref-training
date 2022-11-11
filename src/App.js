2fdea99413561549fd20c0afbba1f461a5902947
Sara Rossow <rossowka@icloud.com>
2022-11-11 13:48:54 +0100

    fix: moved logic to parent component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useRef, useEffect } from 'react';
 import './App.css';
 import Button from './components/Button';
 
 function App() {
+  const btnRef = useRef(null);
+
+  useEffect(() => {
+      btnRef.current.style.background = "red";
+      btnRef.current.style.color = "white";
+  }, []);
+
+  function onClickHandler() {
+      btnRef.current.style.background = 'darkgoldenrod';
+      btnRef.current.innerText = 'Click!';
+  };
+
+  function onMouseOverHandler() {
+      btnRef.current.style.color = 'black';
+      btnRef.current.style.background = 'gold';
+      btnRef.current.innerText = 'Eee! A mouse!';
+  }
+
+  function onMouseOutHandler() {
+      btnRef.current.innerText = 'But wait..';
+      btnRef.current.style.background = 'darkgrey';
+      btnRef.current.style.color = 'black';
+  }
+
   return (
-      <Button />
+    <div>
+      <Button
+        ref={btnRef}
+        onClick={onClickHandler}
+        onMouseOver={onMouseOverHandler}
+        onMouseOut={onMouseOutHandler}
+        />
+    </div>
   );
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,37 +1,13 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 
-function Button() {
-    const btnRef = useRef(null);
-
-    useEffect(() => {
-        btnRef.current.style.background = "red";
-        btnRef.current.style.color = "white";
-    }, []);
-
-    function onClickHandler() {
-        btnRef.current.style.background = 'darkgoldenrod';
-        btnRef.current.innerText = 'Click!';
-    };
-
-    function onMouseOverHandler() {
-        btnRef.current.style.color = 'black';
-        btnRef.current.style.background = 'gold';
-        btnRef.current.innerText = 'Eee! A mouse!';
-    }
-
-    function onMouseOutHandler() {
-        btnRef.current.innerText = 'But wait..';
-        btnRef.current.style.background = 'darkgrey';
-        btnRef.current.style.color = 'black';
-    }
-
-    return (
-        <div>
-            <button ref={btnRef} onClick={onClickHandler} onMouseOver={onMouseOverHandler} onMouseOut={onMouseOutHandler}>
-                Lock and load!
-            </button>
-        </div>
-    );
-}
+const Button = React.forwardRef((props, ref) => (
+    <button
+        ref={ref}
+        onClick={props.onClick}
+        onMouseOver={props.onMouseOver}
+        onMouseOut={props.onMouseOut}>
+        Lock and load!
+    </button>
+));
 
 export default Button;
