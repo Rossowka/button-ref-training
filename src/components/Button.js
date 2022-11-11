924e31dfbcaf51486b5d264633d30121c41d628e
Sara Rossow <rossowka@icloud.com>
2022-11-11 12:50:31 +0100

    added refs, color and text change functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 function Button() {
+    const btnRef = useRef(null);
+
+    useEffect(() => {
+        btnRef.current.style.background = "red";
+        btnRef.current.style.color = "white";
+    }, []);
+
+    function onClickHandler() {
+        btnRef.current.style.background = 'darkgoldenrod';
+        btnRef.current.innerText = 'Click!';
+    };
+
+    function onMouseOverHandler() {
+        btnRef.current.style.color = 'black';
+        btnRef.current.style.background = 'gold';
+        btnRef.current.innerText = 'Eee! A mouse!';
+    }
+
+    function onMouseOutHandler() {
+        btnRef.current.innerText = 'But wait..';
+        btnRef.current.style.background = 'darkgrey';
+        btnRef.current.style.color = 'black';
+    }
+
     return (
         <div>
-            <button>
-                Click!
+            <button ref={btnRef} onClick={onClickHandler} onMouseOver={onMouseOverHandler} onMouseOut={onMouseOutHandler}>
+                Lock and load!
             </button>
         </div>
     );
